da6c75fecbf69db39f6264547af2090ce2c491f2
asm543 <48339623+asm543@users.noreply.github.com>
2022-05-24 17:11:24 +0900

    업비트와 바이낸스 가격 조회 통합, 심볼CRUD 추가, 잔버그 수정

--- findCryptoInfo/upbit/response.js
+++ findCryptoInfo/upbit/response.js
@@ -4,9 +4,27 @@
 라이선스 : CCL BY-NC 4.0
 업비트에서 암호화페 가격 정보를 뜯어와요
 */
+const uppath = '/storage/emulated/0/bot/upbitSymbol.txt'
+const bipath = '/storage/emulated/0/bot/binanceSymbol.txt'
+const fs = FileStream;
 
-function numEstablish(num) {
-  return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+var upSymbol = JSON.parse(fs.read(uppath));
+var biSymbol = JSON.parse(fs.read(bipath));
+
+function addUpSymbol(cmd) {
+    upSymbol[cmd[1]] = cmd[2].toUpperCase();
+}
+
+function getUpSymbol(name) {
+    return upSymbol[name];
+}
+
+function addBiSymbol(cmd) {
+    biSymbol[cmd[1]] = cmd[2].toUpperCase();
+}
+
+function getBiSymbol(name) {
+    return biSymbol[name];
 }
 
 function getCoinMark(name) {
@@ -18,135 +36,154 @@ function getCoinMark(name) {
     return null;
 };
 
-function getCoinName(coinName){
-    switch(coinName){
-        case "비트":
-        case "대장":
-        case "비코":
-        case "빗코":
-          return "비트코인";
-        case "이더":
-          return "이더리움";
-        case "디센":
-          return "디센트럴랜드";
-        case "엑시":
-          return "엑시인피니티";
-        case "알고":
-          return "알고랜드";
-        case "메타":
-          return "메타디움";
-        case "카이버":
-          return "카이버네트워크";
-        case "니어":
-          return "니어프로토콜";
-        case "도지":
-        case "개새끼":
-          return "도지코인";
-        case "이클":
-        case "etc":
-        case "ETC":
-          return "이더리움클래식";
-        case "피르마":
-          return "피르마체인";
-        case "아발":
-          return "아발란체";
-        case "아다":
-        case "카르다노":
-          return "에이다";
-        case "폴":
-          return "폴리곤";
-        case "엔진":
-        case "은진":
-          return "엔진코인";
-        case "1인치":
-          return "1인치네트워크";
-        case "모스":
-          return "모스코인";
-        case "누싸":
-        case "누":
-          return "누사이퍼";
-        case "슨트":
-          return "스테이터스네트워크토큰";
-        case "에브리":
-          return "에브리피디아";
-        case "무비":
-          return "무비블록";
-        case "펀디":
-        case "응디":
-          return "펀디엑스";
-        case "룸":
-          return "룸네트워크";
-        case "썬더":
-          return "썬더코어";
-        case "그로스톨":
-          return "그로스톨코인";
-        case "비토":
-          return "비트토렌트";
-        case "센티넬":
-          return "센티넬프로토콜";
-        case "던":
-        case "던파":
-          return "던프로토콜";
-        case "라이트":
-        case "라코":
-          return "라이트코인";
-        case "스텔라":
-          return "스텔라루멘";
-        case "리퍼":
-          return "리퍼리움";
-        case "스트라":
-          return "스트라티스";
-        case "시아":
-          return "시아코인";
-        case "비캐":
-          return "비트코인캐시";
-        case "코박":
-          return "코박토큰";
-        case "스달":
-          return "스팀달러";
-        case "스붕이":
-          return "비트코인에스브이";
-        case "비골":
-          return "비트코인골드";
-        default:
-          return coinName;
+function getBinancePrice(symbol) {
+    var data = Utils.parse("https://api.binance.com/api/v3/avgPrice?symbol=" + symbol).text();
+    return Number(JSON.parse(data).price);
+};
+
+function getKRWUSDPrice() {
+    var data = Utils.parse("https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD").text();
+    return Number(JSON.parse(data)[0].basePrice);
+};
+
+function getDunamuFng() {
+    var data = JSON.parse(Utils.parse("https://datavalue.dunamu.com/api/fearindex").text());
+    return Number(data.today.score).toFixed(2) + " - " + data.today.stage + "\n" + data.today.comment;
+};
+
+function dotRegex(num, fixed) {
+    return num.toFixed(fixed)
+};
+
+function numEstablish(num) {
+    num = (num + "").split(".");
+    if (num.length > 1) {
+        return num[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "." + num[1];
+    } else return num[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+function upbitCalc(cmd) {
+    var mark = getUpSymbol(cmd[2]);
+    if (mark == undefined || mark == null) {
+        return "cannotFindSymbol";
+    } else if (!Number(cmd[1])) {
+        return "NaN";
+    } else {
+        var data = Utils.parse(
+            "https://api.upbit.com/v1/ticker?markets=" + mark
+        ).text();
+        data = JSON.parse(data);
+        return numEstablish("" + data[0].trade_price * Number(cmd[1]));
     }
 }
 
-function upbitCalc(cmd) {
-  var calc = 0;
-  var mark = getCoinMark(getCoinName(cmd[2]));
-  if (mark == null) {
-    return cmd[2] + "(이)라는 암호화폐를 찾을 수 없습니다.";
-  } else if (!Number(cmd[1])) {
-    return "숫자를 입력해주세요.";
-  } else {
-      var data = Utils.parse(
-      "https://api.upbit.com/v1/ticker?markets=" + mark
-    ).text();
-    data = JSON.parse(data);
-    return numEstablish("" + (data[0].trade_price * Number(cmd[1]))) + "원";
-  }
+function binanceCalc(cmd) {
+    var binancePrice = getBinancePrice(getBiSymbol(cmd[2]));
+    if (getBiSymbol(cmd[2]) == undefined || getBiSymbol(cmd[2]) == null) {
+        return "cannotFindSymbol";
+    } else if (!Number(cmd[1])) {
+        return "NaN";
+    } else {
+        return numEstablish("" + dotRegex(cmd[1] * binancePrice * getKRWUSDPrice(), 0)) + "원 (" + dotRegex(cmd[1] * binancePrice, 3) + "USD)";
+    }
 }
 
 function response(room, msg, sender, isGroupChat, replier) {
     var cmd = msg.split(" ");
-    switch(cmd[0]){
-      case "업": {
-        var mark = getCoinMark(getCoinName(cmd[1]));
-        if (mark == null) {
-            replier.reply(cmd[1] + "(이)라는 암호화폐를 찾을 수 없습니다.");
-            break;
-        } else {
-            var data = Utils.parse("https://api.upbit.com/v1/ticker?markets=" + mark).text();
-            data = JSON.parse(data);
-            replier.reply("현재 " + getCoinName(cmd[1]) + " 시세는 " + numEstablish("" + data[0].trade_price) + "원입니다.");
+    switch (cmd[0]) {
+        case "업": {
+            var mark = getUpSymbol(cmd[1]);
+            if (mark == null) {
+                replier.reply(cmd[1] + "(이)라는 암호화폐를 찾을 수 없습니다.\n심볼을 마켓명-심볼명 형식으로 등록해주세요.\nex) 업추가 비트 KRW-BTC");
+                break;
+            } else {
+                var data = Utils.parse("https://api.upbit.com/v1/ticker?markets=" + mark).text();
+                var text = "원 입니다.";
+                data = JSON.parse(data);
+                switch (mark.split("-")[0]) {
+                    case "KRW":
+                        text = "원 입니다.";
+                        break;
+                    case "BTC":
+                        text = "BTC 입니다.";
+                        break;
+                    case "USDT":
+                        text = "USDT 입니다.";
+                        break;
+                }
+                replier.reply(getUpSymbol(cmd[1]) + "의 현재 가격은 " + numEstablish(data[0].trade_price) + text);
+                break;
+            }
+        }
+        case "업심볼": {
+            replier.reply(JSON.stringify(upSymbol));
             break;
         }
-      }
-      case "계산": {
-        replier.reply("= " + upbitCalc(cmd));
-        break;
-      }
-   }
+        case "업추가": {
+            addUpSymbol(cmd);
+            fs.write(uppath, JSON.stringify(upSymbol));
+            replier.reply("업비트 심볼 " + cmd[1] + " = " + cmd[2] + " 추가되었습니다.");
+            break;
+        }
+        case "업수정": {
+            addUpSymbol(cmd);
+            fs.write(uppath, JSON.stringify(upSymbol));
+            replier.reply("업비트 심볼 " + cmd[1] + " = " + cmd[2] + " 수정되었습니다.");
+            break;
+        }
+        case "업삭제": {
+            delete upSymbol[cmd[1]];
+            fs.write(uppath, JSON.stringify(upSymbol));
+            replier.reply("업비트 심볼 " + cmd[1] + " 삭제되었습니다.");
+            break;
+        }
+        case "바심볼": {
+            replier.reply(JSON.stringify(biSymbol));
+            break;
+        }
+        case "바추가": {
+            addBiSymbol(cmd);
+            fs.write(bipath, JSON.stringify(biSymbol));
+            replier.reply("바이낸스 심볼 " + cmd[1] + " = " + cmd[2] + " 추가되었습니다.");
+            break;
+        }
+        case "바수정": {
+            addBiSymbol(cmd);
+            fs.write(bipath, JSON.stringify(biSymbol));
+            replier.reply("바이낸스 심볼 " + cmd[1] + " = " + cmd[2] + " 수정되었습니다.");
+            break;
+        }
+        case "바삭제": {
+            delete biSymbol[cmd[1]];
+            fs.write(bipath, JSON.stringify(biSymbol));
+            replier.reply("바이낸스 심볼 " + cmd[1] + " 삭제되었습니다.");
+            break;
+        }
+        case "계산": {
+            var upbitPrice = upbitCalc(cmd);
+            var binancePrice = binanceCalc(cmd);
+            if (upbitPrice != "cannotFindSymbol" && upbitPrice != "NaN") {
+                replier.reply("= " + upbitPrice + "원");
+            } else if (binancePrice != "cannotFindSymbol" && binancePrice != "NaN") {
+                replier.reply("= " + binancePrice);
+            } else if (upbitPrice == "cannotFindSymbol" && binancePrice == "cannotFindSymbol") {
+                replier.reply("= 심볼을 찾을 수 없습니다.");
+            } else if (upbitPrice == "NaN" && binancePrice == "NaN") {
+                replier.reply("= 숫자를 입력해주세요.");
+            } else replier.reply("= 계산 1 코인명 형식으로 입력해주세요.");
+            break;
+        }
+        case "바": {
+            if (getBiSymbol(cmd[1]) == undefined || getBiSymbol (cmd[1]) == null) {
+                replier.reply(cmd[1] + "(이)라는 암호화폐를 찾을 수 없습니다.\n심볼을 심볼명+거래단위 형식으로 등록해주세요.\nex) 바추가 비트 BTCBUSD");
+            } else {
+                replier.reply("바이낸스의 " + getBiSymbol(cmd[1]) + "가격 : " + numEstablish(dotRegex(getBinancePrice(getBiSymbol(cmd[1])), 3)) + "달러입니다.\n(" + numEstablish(dotRegex(getKRWUSDPrice() * getBinancePrice(getBiSymbol(cmd[1])), 3)) + "원)");
+            }
+            break;
+        }
+        case "지수": {
+            replier.reply(getDunamuFng());
+            break;
+        }
+    }
 }
