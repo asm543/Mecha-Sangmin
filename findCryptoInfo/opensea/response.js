6d9d38ea6a13e5ce886a5700a57f3171e3d648e2
asm543 <48339623+asm543@users.noreply.github.com>
2022-05-24 17:12:38 +0900

    불필요 코드 제거

--- findCryptoInfo/opensea/response.js
+++ findCryptoInfo/opensea/response.js
@@ -49,6 +49,16 @@ function openSea(cmd) {
   }
 }
 
+function openSeaLink(cmd) {
+  var mark = getSlug(cmd[1]);
+  if (mark == null || mark == undefined) {
+    return "컬렉션을 추가해주세요. ex)추가 케이팝 kpopctzen-official\nhttps://opensea.io/collection/kpopctzen-official 에서 kpopctzen-official를 추가해주시면 됩니다.";
+  } else {
+    var data = "https://opensea.io/collection/" + getSlug(cmd[1]);
+    return data;
+  }
+}
+
 function response(room, msg, sender, isGroupChat, replier) {
   var cmd = msg.split(" ");
   switch(cmd[0]){
@@ -60,6 +70,10 @@ function response(room, msg, sender, isGroupChat, replier) {
     }
     case "옾": {
       replier.reply(openSea(cmd));
+      break;
+    }
+    case "옾링": {
+      replier.reply(openSeaLink(cmd));
       break;
     }
     case "슬러그": {
