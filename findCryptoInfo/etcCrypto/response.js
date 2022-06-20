a92cdcee4615a1467e940faf73e633374d91ff50
asm543 <48339623+asm543@users.noreply.github.com>
2022-06-20 18:28:05 +0900

    오타수정

--- findCryptoInfo/etcCrypto/response.js
+++ findCryptoInfo/etcCrypto/response.js
@@ -1,6 +1,3 @@
-https://dexata.kr/#/?tokenA=0xe91ffe2e15ccd56b1b8ddf7cdf848dfee6b5a858&tokenB=
-
-
 function selectDex() {
 
   var temp = org.jsoup.Jsoup.connect("https://dexata.kr/#/?tokenA=0xe91ffe2e15ccd56b1b8ddf7cdf848dfee6b5a858&tokenB=").ignoreContentType(true).get().text();
