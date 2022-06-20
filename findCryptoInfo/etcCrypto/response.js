3973704ec1fce5e1e1c36ad772b611b2b21c9c51
asm543 <48339623+asm543@users.noreply.github.com>
2022-06-20 18:27:40 +0900

    덱스 조회 테스트

--- findCryptoInfo/etcCrypto/response.js
+++ findCryptoInfo/etcCrypto/response.js
@@ -1,1 +1,16 @@
 https://dexata.kr/#/?tokenA=0xe91ffe2e15ccd56b1b8ddf7cdf848dfee6b5a858&tokenB=
+
+
+function selectDex() {
+
+  var temp = org.jsoup.Jsoup.connect("https://dexata.kr/#/?tokenA=0xe91ffe2e15ccd56b1b8ddf7cdf848dfee6b5a858&tokenB=").ignoreContentType(true).get().text();
+  //temp = JSON.parse(temp);
+  //temp = temp.blockPrices[0].baseFeePerGas;
+  return "현재 덱스 : " + temp;
+}
+
+function response(room, msg, sender, isGroupChat, replier) {
+  if(msg == "덱스") {
+    replier.reply(selectDex());
+  }
+}
