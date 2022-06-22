1e690d76ffff3a65a0a7321ac9cf1bc0eebfd649
asm543 <48339623+asm543@users.noreply.github.com>
2022-06-22 12:45:23 +0900

    오픈씨 레어리티 조회 추가

--- findCryptoInfo/opensea/response.js
+++ findCryptoInfo/opensea/response.js
@@ -1,14 +1,22 @@
-const path='/storage/emulated/0/bot/slug.txt'
+const path = '/storage/emulated/0/bot/slug.txt'
+const popolPath = '/storage/emulated/0/bot/popol.txt'
+const rarePath = '/storage/emulated/0/bot/rare.txt'
 const fs = FileStream;
 
 var slug = JSON.parse(fs.read(path));
+var popol = JSON.parse(fs.read(popolPath));
+var rare = JSON.parse(fs.read(rarePath));
 
 function numEstablish(num) {
-  return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  if(num == null || num == undefined){
+    return num;
+  } else return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function dotRegex(num, fixed) {
-  return num.toFixed(fixed)
+  if(Number(num)){
+    return num.toFixed(fixed);
+  } else return num;
 }
 
 function getBinancePrice(symbol) {
@@ -34,64 +42,141 @@ function addSlug(cmd) {
 }
 
 function getSlug(name) {
-  return slug[name];
+  if (slug[name] == null || slug[name] == undefined) {
+    return "컬렉션을 추가해주세요. ex)추가 케이팝 kpopctzen-official\nhttps://opensea.io/collection/kpopctzen-official 에서 kpopctzen-official를 추가해주시면 됩니다.";
+  } else return slug[name];
+}
+
+function addRare(cmd) {
+  rare[cmd[1]] = cmd[2];
+}
+
+function getRare(name) {
+  if (rare[name] == null || rare[name] == undefined) {
+    return "레어리티를 보고싶은 컬렉션의 데이터주소를 토큰번호 제외하고 추가해주세요. ex)추가 케이팝 https://rowoonlabs.mypinata.cloud/ipfs/QmdLgdpuCbohDDkYMyk2rTLo2oFBYSUYcarBvknPqC15ie/";
+  } else return rare[name];
+}
+
+function addPopol(user, cmd) {
+  if (popol[user] == null) {
+    popol[user] = {};
+  }
+  if (slug[cmd[1]] == null || slug[cmd[1]] == undefined) {
+    return "컬렉션을 추가해주세요. ex)추가 케이팝 kpopctzen-official\nhttps://opensea.io/collection/kpopctzen-official 에서 kpopctzen-official를 추가해주시면 됩니다.";
+  } else {
+    popol[user][cmd[1]] = getSlug(cmd[1]);
+  }
+}
+
+function getPopol(user) {
+  return popol[user];
 }
 
 function openSea(cmd) {
-  var mark = getSlug(cmd[1]);
-  if (mark == null || mark == undefined) {
+  var mark = getSlug(cmd);
+  if (slug[cmd] == null || slug[cmd] == undefined) {
     return "컬렉션을 추가해주세요. ex)추가 케이팝 kpopctzen-official\nhttps://opensea.io/collection/kpopctzen-official 에서 kpopctzen-official를 추가해주시면 됩니다.";
   } else {
     var data = Utils.parse("https://api.opensea.io/api/v1/collection/" + mark).text();
     data = JSON.parse(data);
     var responseData = data.collection.stats
-    return cmd[1] + "의 오픈씨 거래 정보\n일 거래량 : " + dotRegex(responseData.one_day_volume, 3) + "ETH\n일 거래 횟수 : " + responseData.one_day_sales + "\n평균가격 : ⓔ " + dotRegex(responseData.average_price, 3) + " ⓚ " + dotRegex(getKlayPrice(responseData.average_price), 0) + "\n바닥가 : ⓔ " + dotRegex(responseData.floor_price, 3) + " ⓚ " + dotRegex(getKlayPrice(responseData.floor_price), 0) + "\n원화 : " + numEstablish(dotRegex(getKRWPrice(responseData.floor_price), 0)) + "원";
+    return cmd + "의 오픈씨 거래 정보\n일 거래량 : " + dotRegex(responseData.one_day_volume, 3) + "ETH\n일 거래 횟수 : " + responseData.one_day_sales + "\n평균가격 : ⓔ " + dotRegex(responseData.average_price, 3) + " ⓚ " + dotRegex(getKlayPrice(responseData.average_price), 0) + "\n바닥가 : ⓔ " + dotRegex(responseData.floor_price, 3) + " ⓚ " + dotRegex(getKlayPrice(responseData.floor_price), 0) + "\n원화 : " + numEstablish(dotRegex(getKRWPrice(responseData.floor_price), 0)) + "원";
   }
 }
 
 function openSeaLink(cmd) {
-  var mark = getSlug(cmd[1]);
-  if (mark == null || mark == undefined) {
+  if (slug[cmd] == null || slug[cmd] == undefined) {
     return "컬렉션을 추가해주세요. ex)추가 케이팝 kpopctzen-official\nhttps://opensea.io/collection/kpopctzen-official 에서 kpopctzen-official를 추가해주시면 됩니다.";
   } else {
-    var data = "https://opensea.io/collection/" + getSlug(cmd[1]);
+    var data = "https://opensea.io/collection/" + getSlug(cmd);
     return data;
   }
 }
 
-function openSeaApi(cmd) {
-  var mark = getSlug(cmd[1]);
-  if (mark == null || mark == undefined) {
-    return "컬렉션을 추가해주세요. ex)추가 케이팝 kpopctzen-official\nhttps://opensea.io/collection/kpopctzen-official 에서 kpopctzen-official를 추가해주시면 됩니다.";
+function openSeaRare(cmd) {
+  var mark = getRare(cmd[1]);
+  var returnData = "";
+  if(cmd.length < 4) {
+    return "오류 - 레어 '검색할 컬렉션명' '검색할 속성명' 토큰번호 토큰번호 토큰번호 .... 순으로 입력해주세요.";
+  } else if (mark == null || mark == undefined) {
+    return "레어리티를 보고싶은 컬렉션의 데이터주소를 토큰번호 제외하고 추가해주세요. ex)추가 케이팝 https://rowoonlabs.mypinata.cloud/ipfs/QmdLgdpuCbohDDkYMyk2rTLo2oFBYSUYcarBvknPqC15ie/";
   } else {
-    var data = "https://api.opensea.io/api/v1/collection/" + getSlug(cmd[1]);
-    return data;
+    cmd.forEach(token => {
+      if(Number(token)){
+        var data = Utils.parse(mark + token).text();
+        data = JSON.parse(data);
+        data.attributes.forEach(item => {
+          if(item.trait_type == cmd[2]){
+            returnData += token + " : " + item.value + "\n";
+          }
+        })
+      }
+    })
   }
+  return returnData;
 }
 
 function response(room, msg, sender, isGroupChat, replier) {
   var cmd = msg.split(" ");
-  switch(cmd[0]){
-    case "추가": {
-      addSlug(cmd);
-      fs.write(path, JSON.stringify(slug));
-      replier.reply(cmd[1] + " 추가되었습니다.");
-      break;
-    }
+  switch (cmd[0]) {
     case "옾": {
-      replier.reply(openSea(cmd));
+      replier.reply(openSea(cmd[1]));
       break;
     }
     case "옾링": {
-      replier.reply(openSeaLink(cmd));
+      replier.reply(openSeaLink(cmd[1]));
       break;
     }
-    case "옾데": {
-      replier.reply(openSeaApi(cmd));
+    case "레어": {
+      replier.reply(openSeaRare(cmd));
+      break;
+    }
+    case "레어추가": {
+      addRare(cmd);
+      fs.write(path, JSON.stringify(rare));
+      replier.reply(cmd[1] + "의 레어리티검색 추가되었습니다.");
+      break;
+    }
+    case "레어수정": {
+      rare[cmd[1]] = cmd[2];
+      fs.write(path, JSON.stringify(rare));
+      replier.reply(cmd[1] + " 수정되었습니다.");
+      break;
+    }
+    case "레어삭제": {
+      delete rare[cmd[1]];
+      fs.write(path, JSON.stringify(rare));
+      replier.reply(cmd[1] + " 삭제되었습니다.");
+      break;
+    }
+    case "옾폴": {
+      var returnText = sender + "님의 옾폴현황\n -------------------------- \n";
+      for (slugName in popol[sender]) {
+          returnText = returnText + openSea(slugName) + "\n -------------------------- \n";
+      }
+      replier.reply(returnText);
+      break;
+    }
+    case "옾폴링": {
+      var returnText = "";
+      for (slugName in popol[sender]) {
+          returnText = returnText + openSeaLink(slugName) + "\n";
+      }
+      replier.reply(returnText);
+      break;
+    }
+    case "내옾폴": {
+      replier.reply(JSON.stringify(popol));
       break;
     }
     case "슬러그": {
       replier.reply(JSON.stringify(slug));
+      break;
+    }
+    case "추가": {
+      addSlug(cmd);
+      fs.write(path, JSON.stringify(slug));
+      replier.reply(cmd[1] + " 추가되었습니다.");
       break;
     }
     case "수정": {
@@ -102,6 +187,25 @@ function response(room, msg, sender, isGroupChat, replier) {
     case "삭제": {
       delete slug[cmd[1]];
       fs.write(path, JSON.stringify(slug));
+      break;
+    }
+    case "옾폴추가": {
+      addPopol(sender, cmd);
+      fs.write(popolPath, JSON.stringify(popol));
+      replier.reply(sender + "님의 옾폴에 " + cmd[1] + "가 추가되었습니다.\n(등록된 슬러그 : " + slug[cmd[1]] + ")");
+      break;
+    }
+    case "옾폴수정": {
+      delete popol[sender][cmd[1]];
+      popol[sender][cmd[2]] = slug[cmd[2]];
+      fs.write(popolPath, JSON.stringify(popol));
+      replier.reply(sender + "님의 옾폴에서 " + cmd[1] + "(이)가 수정되었습니다. \n(수정된 슬러그 : " + slug[cmd[2]] + ")");
+      break;
+    }
+    case "옾폴삭제": {
+      delete popol[sender][cmd[1]];
+      fs.write(popolPath, JSON.stringify(popol));
+      replier.reply(sender + "님의 옾폴에서 " + cmd[1] + "(이)가 삭제되었습니다.");
       break;
     }
   }
